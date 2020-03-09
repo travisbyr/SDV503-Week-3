5eae7ba0d27af9995f07c599ab215e5fbb60e68d
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-03-09 13:37:27 +1300

    fileUpdate

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
+/*
 let x = 10;
 let y = 5;
-
 console.log(x + y); // Addtion
 console.log(x - y); // Suubtraction
 console.log(x * y); // Multiplication
 console.log(x / y); // Divide
 console.log(x % y); // Remainder
 console.log(x ** y); // Double Multiplication 
+*/
+/*
+let x = 10; // Activiy 1.1
+console.log(x);
+console.log(++x);
+console.log(x);
+console.log(++x);
+console.log(x);
+console.log(++x);
+console.log(x);
+*/
+/*
+let x = 10 
+let y = x;
+console.log("Pre - Increment");
+y = ++x;
+console.log(x);
+console.log(y);
+console.log("Post- Increment");
+y = x++;
+console.log(x);
+console.log(y);
+*/
+
+let x = 10; // Activity 1.2
+console.log(x);
+console.log(x--);
+console.log(x);
+console.log(x--);
+console.log(x);
+console.log(x--);
+console.log(x);
