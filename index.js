1586aed66c632111f3890d47952eaeb95e184cbe
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-03-10 08:54:55 +1300

    Update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ----- 9/3/20 -----
 /*
 let x = 10;
 let y = 5;
@@ -108,7 +109,7 @@ const secondWord = "World";
 console.log(firstWord + secondWord);
 console.log(`${firstWord} ${secondWord}`); //Efficent to use in code
 */
-
+/*
 console.log(5 + 10);
 console.log(5 + '10');
 console.log('10' + 5);
@@ -120,3 +121,5 @@ console.log(10 - '5');
 console.log(10 * '5');
 console.log(10 + 10 - '5');
 console.log('10' / '10');
+
+// ----- 10/3/20 -----
