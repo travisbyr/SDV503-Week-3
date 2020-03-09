5c79b0626997ffa5b48cc25a93452b47f3d1d679
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-03-09 13:18:24 +1300

    update

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Journel Entry 3
 
-What Why How
 
-Markdown | Less | Pretty
+
+### What | Why | How
 --- | --- | ---
 *Still* | `renders` | **nicely**
 1 | 2 | 3
---
+++ index.js
@@ -0,0 +1,9 @@
+let x = 10;
+let y = 5;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
